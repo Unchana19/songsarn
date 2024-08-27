1874b2551613210f97274386863c048d24f2c6b4
annchana <annchanaoat@gmail.com>
2024-08-27 22:57:32 +0700

    init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+interface Props {}
+
+const HomePage: NextPage<Props> = () => {
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
---
+++ components/navbar.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface Props {}
+
+const NavbarComponent: FC<Props> = (): JSX.Element => {
+  return <div>Navbar</div>;
+};
+
+export default NavbarComponent;
