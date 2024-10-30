b0bee6058d02b8ec760c5bfd3677a8a16b8e4283
annchana <annchanaoat@gmail.com>
2024-10-30 17:48:41 +0700

    fix max detail product

--- lib/schemas/createProductSchema.ts
+++ lib/schemas/createProductSchema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const createProductSchema = z.object({
   category: z.string(),
   name: z.string().min(1).max(20),
-  detail: z.string().min(1).max(20),
+  detail: z.string().min(1).max(50),
   price: z.string().regex(/^\d+(\.\d+)?$/, "Quantity must be a number"),
 });
 
